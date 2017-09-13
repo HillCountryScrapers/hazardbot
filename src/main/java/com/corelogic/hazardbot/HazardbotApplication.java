4f35f6e4e00f9da1ed620edd197f47b9436c96b3
Robert Ottolia <rottolia@corelogic.com>
2017-09-13 11:49:55 -0500

    Add basic DM responder
    Signed-off-by: Robert Ottolia <rottolia@corelogic.com>
    Signed-off-by: Jonathan Lin <jlin@corelogic.com>

--- src/main/java/com/corelogic/hazardbot/HazardbotApplication.java
+++ src/main/java/com/corelogic/hazardbot/HazardbotApplication.java
@@ -3,7 +3,7 @@ package com.corelogic.hazardbot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"me.ramswaroop.jbot", "com.corelogic.hazardbot"})
 public class HazardbotApplication {
 
 	public static void main(String[] args) {
