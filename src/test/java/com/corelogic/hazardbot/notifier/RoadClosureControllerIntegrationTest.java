181ab11848483e75c0ba2125849571701e1d74fd
Kathy Ellison <kaellison@corelogic.com>
2017-09-13 14:44:12 -0500

    Active mock profile so Spring Context will load
    Signed-off-by: Bruce Thelen <bthelen@corelogic.com>

--- src/test/java/com/corelogic/hazardbot/notifier/RoadClosureControllerIntegrationTest.java
+++ src/test/java/com/corelogic/hazardbot/notifier/RoadClosureControllerIntegrationTest.java
@@ -7,6 +7,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -18,6 +19,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
+@ActiveProfiles("mock")
 @SpringBootTest(classes = HazardbotApplication.class)
 public class RoadClosureControllerIntegrationTest {
 
