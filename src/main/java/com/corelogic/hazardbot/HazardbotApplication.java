47a7422d769a454966ca09d892b538e413bef171
Jonathan Lin <jlin@corelogic.com>
2017-09-13 10:46:04 -0500

    Initial commit
    Signed-off-by: Grant Hutchins <ghutchins@pivotal.io>

---
+++ src/main/java/com/corelogic/hazardbot/HazardbotApplication.java
@@ -0,0 +1,12 @@
+package com.corelogic.hazardbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HazardbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HazardbotApplication.class, args);
+	}
+}
