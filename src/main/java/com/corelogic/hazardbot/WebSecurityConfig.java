2d41c18af51bb947abdb75f80c859cc366712511
Rajasimhan Varadharajan <rvaradharajan@corelogic.com>
2017-09-14 16:56:36 -0500

    Disable CSRF
    Signed-off-by: Grant Hutchins <ghutchins@pivotal.io>

--- src/main/java/com/corelogic/hazardbot/WebSecurityConfig.java
+++ src/main/java/com/corelogic/hazardbot/WebSecurityConfig.java
@@ -10,6 +10,8 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+
         http
             .authorizeRequests()
             .anyRequest().permitAll();
